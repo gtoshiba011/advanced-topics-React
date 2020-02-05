98c4640cda27ccafd2ce4ab85fa73709a8098d0e
Howard Chen <pohaoche@usc.edu>
2020-02-05 01:04:06 -0800

    call backend API in useEffect

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import Movie from "./hoc/Movie";
 import Counter from "./hooks/counter";
+import Users from "./hooks/users";
 
 function App() {
   // return <Movie />;
-  return <Counter />;
+  // return <Counter />;
+  return <Users />;
 }
 
 export default App;
---
+++ src/hooks/users.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Users = props => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(data);
+    }
+    getUsers();
+  });
+  return (
+    <div>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
