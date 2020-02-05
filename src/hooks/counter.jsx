2e9ba00c21b97f3101bdfbd9825af8ccf9cfe6c9
Howard Chen <pohaoche@usc.edu>
2020-02-05 00:46:23 -0800

    example of useEffect

--- src/hooks/counter.jsx
+++ src/hooks/counter.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter = props => {
   // useState(0)[0] ~ this.state.count
   // useState(0)[0] ~ this.setState()
   const [count, setCount] = useState(0);
-  // second state variable
-  const [name, setName] = useState("");
+  const [name, setName] = useState("Someone");
+
+  useEffect(() => {
+    // function is called every time the component is rendered
+    document.title = `${name} has clicked ${count} times!`;
+    return () => {
+      console.log("clean up");
+    };
+  });
+  // componentDidMount
+  // componentDidUpdate
+  // componentWillUnmount
 
   return (
     <React.Fragment>
